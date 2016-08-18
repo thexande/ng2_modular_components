34166a27860c7180a04568a340a006cfd833ac34
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-18 17:59:25 -0600

    like button component, voting button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { LikeComponent } from './like/like.component'
+import { VoteComponent } from './vote/vote.component'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives:[LikeComponent, VoteComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Modular Angular 2 Components';
+  onLikeChange($event) {
+    console.log($event);
+  }
 }
